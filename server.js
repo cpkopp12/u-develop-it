93e44af7c6c4194e7d09ef7b86ac7a59e65b8c49
cpkopp <cameronpkopp@gmail.com>
2022-10-17 12:40:53 -1000

    created db queries

--- server.js
+++ server.js
@@ -20,10 +20,35 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
-//test
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+
+//query: single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
 });
+
+//query: delete candidate
+/*
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+*/
+//query: create candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 
 //ROUTES------------------------------------------------------------------
 
