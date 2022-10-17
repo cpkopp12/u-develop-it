75a3dbfbc44c0c5cc9f6bb548f6d5ac0ad101416
cpkopp <cameronpkopp@gmail.com>
2022-10-17 12:20:56 -1000

    connected server to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-//IMPORTS: express------------------------------------------
+//IMPORTS: express, mysql2------------------------------------------
 const express = require('express');
+const mysql = require('mysql2');
 
 //DESIGNATE PORT + APP----------------------------------------
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,21 @@ const app = express();
 //EXPRESS MIDDLEWARE--------------------------------------------
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//CONNECT DATABASE-----------------------------------------------------
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Cammatt12!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+//test
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //ROUTES------------------------------------------------------------------
 
