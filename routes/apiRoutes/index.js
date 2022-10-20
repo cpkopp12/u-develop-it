7831f6042d3c4d95d863faf26e3f9772f3a53ce0
cpkopp <cameronpkopp@gmail.com>
2022-10-19 14:11:03 -1000

    voter routes done

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,11 @@
+//DECLARATIONS: express, router -----------------------------------------
 const express = require('express');
 const router = express.Router();
 
+//ROUTER SETUP -----------------------------------------------------------
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
+//EXPORTS ------------------------------------------------------------------
 module.exports = router;
